a217cbb36fd260b32671e49c93d4e1a3444a0347
ccchloe510 <tc1822@ic.ac.uk>
2026-01-29 21:25:44 +0000

    scripts added

---
+++ docs/source/modules/documentation/tutorials/scripts/lab2_1.py
@@ -0,0 +1,112 @@
+checkpoint = "prajjwal1/bert-tiny"
+tokenizer_checkpoint = "bert-base-uncased"
+dataset_name = "imdb"
+
+from chop.tools import get_tokenized_dataset
+
+dataset, tokenizer = get_tokenized_dataset(
+    dataset=dataset_name,
+    checkpoint=tokenizer_checkpoint,
+    return_tokenizer=True,
+)
+
+import torch.nn as nn
+from chop.nn.modules import Identity
+
+search_space = {
+    "num_layers": [2, 4, 8],
+    "num_heads": [2, 4, 8, 16],
+    "hidden_size": [128, 192, 256, 384, 512],
+    "intermediate_size": [512, 768, 1024, 1536, 2048],
+    "linear_layer_choices": [
+        nn.Linear,
+        Identity,
+    ],
+}
+
+from transformers import AutoConfig, AutoModelForSequenceClassification
+from chop.tools.utils import deepsetattr
+
+
+def construct_model(trial):
+    config = AutoConfig.from_pretrained(checkpoint)
+
+    # Update the paramaters in the config
+    for param in [
+        "num_layers",
+        "num_heads",
+        "hidden_size",
+        "intermediate_size",
+    ]:
+        chosen_idx = trial.suggest_int(param, 0, len(search_space[param]) - 1)
+        setattr(config, param, search_space[param][chosen_idx])
+
+    trial_model = AutoModelForSequenceClassification.from_config(config)
+
+    for name, layer in trial_model.named_modules():
+        if isinstance(layer, nn.Linear) and layer.in_features == layer.out_features:
+            new_layer_cls = trial.suggest_categorical(
+                f"{name}_type",
+                search_space["linear_layer_choices"],
+            )
+
+            if new_layer_cls == nn.Linear:
+                continue
+            elif new_layer_cls == Identity:
+                new_layer = Identity()
+                deepsetattr(trial_model, name, new_layer)
+            else:
+                raise ValueError(f"Unknown layer type: {new_layer_cls}")
+
+    return trial_model
+
+from chop.tools import get_trainer
+
+
+def objective(trial):
+
+    # Define the model
+    model = construct_model(trial)
+
+    trainer = get_trainer(
+        model=model,
+        tokenized_dataset=dataset,
+        tokenizer=tokenizer,
+        evaluate_metric="accuracy",
+        num_train_epochs=1,
+    )
+
+    trainer.train()
+    eval_results = trainer.evaluate()
+
+    # Set the model as an attribute so we can fetch it later
+    trial.set_user_attr("model", model)
+
+    return eval_results["eval_accuracy"]
+
+from optuna.samplers import GridSampler, RandomSampler, TPESampler
+
+sampler = TPESampler()
+
+import optuna
+
+study = optuna.create_study(
+    direction="maximize",
+    study_name="bert-tiny-nas-study",
+    sampler=sampler,
+)
+
+study.optimize(
+    objective,
+    n_trials=1,
+    timeout=60 * 60 * 24,
+)
+
+from pathlib import Path
+import dill
+
+model = study.best_trial.user_attrs["model"].cpu()
+
+with open(f"{Path.home()}/tutorial_5_best_model.pkl", "wb") as f:
+    dill.dump(model, f)
+
